bb943f7e6f1c8f620957c80ff57cc85a4250d31a
win1010437630 <1010437630@qq.com>
2017-08-08 17:09:59 +0800

    000

--- src/Content.js
+++ src/Content.js
@@ -23,42 +23,27 @@ class Content extends Component{
         	content:e
     	})
       }.bind(this)
-    })
-    
-    this.refs.replace.onclick=function(){
-      $('.newcon').attr("display",'block');
-      $.ajax({
-          type:"post",
-          url:"http://127.0.0.1:3005/test/replacetable",
-          async:true,
-          data:{
-            'id':id,
-            'content':$('.newcon').val(),
-          },
-          success:function(e){console.log(e)}
-      });
-    }
-
-    this.refs.remove.onclick=function(){       
-      $.ajax({
+    })   
+  }
+  remove(num){
+    $.ajax({
         type:"post",
         url:"http://127.0.0.1:3005/test/removetable",
         async:true,
-        data:{'id': id},
+        data:{'id': num},
         success:function(e){console.log(e)}
       });
-    }
   }
   render(){
     return <ul>
         {this.state.content.map(function(e){
          return <li>
           <span>{e.content}</span>
-          <button ref="replace">修改</button>
-          <button ref="remove">删除</button>
+          <button ref="replace" >修改</button>
+          <button ref="remove" onClick={this.remove.bind(this,(e.id))}>删除</button>
           <input className="newcon" type="text" style={{display: 'none'}}/>
          </li>
-        })}
+        },this)}
     </ul>    
   }
 }
--- src/Title.js
+++ src/Title.js
@@ -20,30 +20,15 @@ class Title extends Component{
     	 })
       }.bind(this)
     });
-
-    /*this.refs.replace.onclick=function(){
-      $('.newtit').attr("display",'block');
-      $.ajax({
-          type:"post",
-          url:"http://127.0.0.1:3005/test/replacetable",
-          async:true,
-          data:{
-            'id':$('.id').html(),
-            'title':$('.newtit').val(),
-          },
-          success:function(e){console.log(e)}
-      });
-    }
-
-    this.refs.remove.onclick=function(){       
-      $.ajax({
+  }
+  remove(num){
+    $.ajax({
         type:"post",
         url:"http://127.0.0.1:3005/test/removetable",
         async:true,
-        data:{'id':$('.id').html()},
+        data:{'id': num},
         success:function(e){console.log(e)}
       });
-    }*/
   }
   render(){
     return <ul>
@@ -52,10 +37,10 @@ class Title extends Component{
           <span className="id">{e.id}</span>
           <Link to={`/content?id=${e.id}`}>{e.title}</Link>
           <button ref="replace">修改</button>
-          <button ref="remove">删除</button>          
+          <button ref="remove" onClick={this.remove.bind(this,(e.id))}>删除</button>          
           <input className="newtit" type="text" style={{display: 'none'}}/>
          </li>
-        })}
+        },this)}
     </ul>    
   }
 }
